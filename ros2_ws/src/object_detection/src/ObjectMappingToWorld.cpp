d3eef370d3b9cda1bf4837d1906d2f64d21f1e18
Emmaa65 <emma.freundt@tum.de>
2026-02-10 22:38:03 +0100

    outline of object detection

---
+++ ros2_ws/src/object_detection/src/ObjectMappingToWorld.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <image_geometry/pinhole_camera_model.h>
+#include <cv_bridge/cv_bridge.h>
+
+
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world object_detection package\n");
+  return 0;
+}
