e741cd41290587c09df1c39911b7262c0ea3b4ce
Emmaa65 <emma.freundt@tum.de>
2026-02-17 15:28:07 +0100

    removed transform from object_detection.launch.py

--- ros2_ws/src/object_detection/launch/object_detection.launch.py
+++ ros2_ws/src/object_detection/launch/object_detection.launch.py
@@ -5,37 +5,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Camera to Quadrotor/TrueState (adjust based on your camera position on the drone)
-    camera_to_body = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=[
-            '--x', '0.0',     
-            '--y', '0.0',
-            '--z', '0.1',   #distance Quadrotor/TrueState center to camera
-            '--roll', '0.0',
-            '--pitch', '0.0',
-            '--yaw', '0.0',
-            '--frame-id', 'Quadrotor/TrueState',
-            '--child-frame-id', 'Quadrotor/Sensors/DepthCamera_optical'
-        ]
-    )
-    
-    # Align depth camera with semantic camera (identity transform)
-    depth_to_semantic = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        arguments=[
-            '--x', '0.0',
-            '--y', '0.0',
-            '--z', '0.0',
-            '--roll', '0.0',
-            '--pitch', '0.0',
-            '--yaw', '0.0',
-            '--frame-id', 'Quadrotor/Sensors/SemanticCamera',
-            '--child-frame-id', 'Quadrotor/Sensors/DepthCamera_optical'
-        ]
-    )
 
     object_mapping_node = Node(
         package='object_detection',
@@ -50,8 +19,6 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
-        camera_to_body,
-        depth_to_semantic,
         object_mapping_node
     ])
 
